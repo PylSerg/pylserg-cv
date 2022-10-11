6f15f71f56e764da40d03699d9d3a98288cf490a
Serhii <39593488+PylSerg@users.noreply.github.com>
2022-10-11 19:56:17 +0300

    deletes import index.css and adds import style.scss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+// eslint-disable-next-line no-unused-vars
+import styles from "./sass/style.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
