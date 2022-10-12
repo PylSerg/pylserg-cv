d7dac95e7ac82a4ec02ac33fe16e44da399f00d2
Serhii <39593488+PylSerg@users.noreply.github.com>
2022-10-12 16:39:39 +0300

    adds typing emulation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,24 @@
-import React from "react";
+import { useState } from "react";
 import Typing from "./Typing";
 import photo from "../assets/photo.webp";
 
 export default function Header() {
+  const [firstMessage, setFirstMessage] = useState(false);
+  const [secondMessage, setSecondMessage] = useState(false);
+  const [thirdMessage, setThirdMEssage] = useState(false);
+
+  setTimeout(() => {
+    setFirstMessage(true);
+  }, 1000);
+
+  setTimeout(() => {
+    setSecondMessage(true);
+  }, 3000);
+
+  setTimeout(() => {
+    setThirdMEssage(true);
+  }, 5000);
+
   return (
     <section className="header">
       <img src={photo} alt="Avatar" width="300" height="300" />
@@ -41,15 +57,38 @@ export default function Header() {
             About me
         */}
 
-      <Typing />
+      {firstMessage && (
+        <div className="message__block">
+          <Typing />
 
-      <p>
-        Hello! I'm Serhii and I'm front-end developer. I don't have much
-        commercial experience, but five years of experience with HTML and CSS,
-        and two years of using JavaScript allows me to handle just about
-        everything. I am looking for a company that has interesting projects,
-        there is an opportunity for further development and professional growth.
-      </p>
+          <p className="message__text">
+            Hello! I'm Serhii and I'm front-end developer.
+          </p>
+        </div>
+      )}
+
+      {secondMessage && (
+        <div className="message__block">
+          <Typing />
+
+          <p className="message__text">
+            I don't have much commercial experience, but five years of
+            experience with HTML and CSS, and two years of using JavaScript
+            allows me to handle just about everything.
+          </p>
+        </div>
+      )}
+
+      {thirdMessage && (
+        <div className="message__block">
+          <Typing />
+
+          <p className="message__text">
+            I am looking for a company that has interesting projects, there is
+            an opportunity for further development and professional growth.
+          </p>
+        </div>
+      )}
     </section>
   );
 }
