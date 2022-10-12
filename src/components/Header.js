f42025d6ee7a24c57e60c6239b6c27339695a46d
Serhii <39593488+PylSerg@users.noreply.github.com>
2022-10-12 21:17:04 +0300

    adds typing animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,67 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Typing from "./Typing";
 import photo from "../assets/photo.webp";
 
 export default function Header() {
+  const [typingMessage, setTypingMessage] = useState(false);
   const [firstMessage, setFirstMessage] = useState(false);
   const [secondMessage, setSecondMessage] = useState(false);
-  const [thirdMessage, setThirdMEssage] = useState(false);
+  const [thirdMessage, setThirdMessage] = useState(false);
 
-  setTimeout(() => {
+  useEffect(() => {
+    setTimeout(() => {
+      showFirstMessage();
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      showSecondMessage();
+    }, 3500);
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      showThirdMessage();
+    }, 6500);
+  }, []);
+
+  function showFirstMessage() {
     setFirstMessage(true);
-  }, 1000);
+    setTypingMessage(true);
 
-  setTimeout(() => {
+    setTimeout(() => {
+      setTypingMessage(false);
+    }, 1250);
+  }
+
+  function showSecondMessage() {
     setSecondMessage(true);
-  }, 3000);
+    setTypingMessage(true);
 
-  setTimeout(() => {
-    setThirdMEssage(true);
-  }, 5000);
+    setTimeout(() => {
+      setTypingMessage(false);
+    }, 1250);
+  }
+
+  function showThirdMessage() {
+    setThirdMessage(true);
+    setTypingMessage(true);
+
+    setTimeout(() => {
+      setTypingMessage(false);
+    }, 1250);
+  }
 
   return (
     <section className="header">
-      <img src={photo} alt="Avatar" width="300" height="300" />
+      <img
+        className="header__photo"
+        src={photo}
+        alt="Avatar"
+        width="300"
+        height="300"
+      />
 
       <div className="header__main">
         <h1>Serhii Pylypenko</h1>
@@ -32,7 +72,7 @@ export default function Header() {
             Contacts 
         */}
 
-        <h3>Contacts</h3>
+        <h3 className="contacts__title">Contacts</h3>
         <ul>
           <li>
             Tel.: <a href="tel:+380967199192">+38 (096) 719 91 92</a>{" "}
@@ -57,38 +97,53 @@ export default function Header() {
             About me
         */}
 
-      {firstMessage && (
-        <div className="message__block">
-          <Typing />
+      <div className="message__container">
+        <h3 className="message__title">
+          About me{" "}
+          {typingMessage && (
+            <span className="message__title-typing">Serhii is typing...</span>
+          )}
+        </h3>
 
-          <p className="message__text">
-            Hello! I'm Serhii and I'm front-end developer.
-          </p>
-        </div>
-      )}
+        {!firstMessage && <p className="message__preload">No messages</p>}
 
-      {secondMessage && (
-        <div className="message__block">
-          <Typing />
+        {firstMessage && (
+          <div className="message__block">
+            <img className="message__avatar" src={photo} alt="Avatar" />
 
-          <p className="message__text">
-            I don't have much commercial experience, but five years of
-            experience with HTML and CSS, and two years of using JavaScript
-            allows me to handle just about everything.
-          </p>
-        </div>
-      )}
+            <Typing />
 
-      {thirdMessage && (
-        <div className="message__block">
-          <Typing />
+            <p className="message__text">
+              Hello! I'm Serhii and I'm front-end developer.
+            </p>
+          </div>
+        )}
+        {secondMessage && (
+          <div className="message__block">
+            <img className="message__avatar" src={photo} alt="Avatar" />
 
-          <p className="message__text">
-            I am looking for a company that has interesting projects, there is
-            an opportunity for further development and professional growth.
-          </p>
-        </div>
-      )}
+            <Typing />
+
+            <p className="message__text">
+              I don't have much commercial experience, but five years of
+              experience with HTML and CSS, and two years of using JavaScript
+              allows me to handle just about everything.
+            </p>
+          </div>
+        )}
+        {thirdMessage && (
+          <div className="message__block">
+            <img className="message__avatar" src={photo} alt="Avatar" />
+
+            <Typing />
+
+            <p className="message__text">
+              I am looking for a company that has interesting projects, there is
+              an opportunity for further development and professional growth.
+            </p>
+          </div>
+        )}
+      </div>
     </section>
   );
 }
