e9ffe1a385d1c58ae1c9e00ae278f902f5689b4c
Serhii <39593488+PylSerg@users.noreply.github.com>
2022-10-12 12:19:06 +0300

    adds styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,40 @@
 import React from "react";
-import photo from "../assets/photo.jpg";
+import photo from "../assets/photo.webp";
 
 export default function Header() {
   return (
     <section className="header">
-      <img src={photo} alt="Avatar" width="300" />
-      <h1>Serhii Pylypenko</h1>
+      <img src={photo} alt="Avatar" width="300" height="300" />
 
-      <h2>Front-End Developer</h2>
+      <div className="header__main">
+        <h1>Serhii Pylypenko</h1>
 
-      {/* 
+        <h2>Front-End Developer</h2>
+
+        {/* 
             Contacts 
         */}
 
-      <h3>Contacts</h3>
-      <ul>
-        <li>
-          Tel.: <a href="tel:+380967199192">+38 (096) 719 91 92</a>{" "}
-          <a href="viber://chat?number=380967199192">[ Viber ]</a>
-        </li>
+        <h3>Contacts</h3>
+        <ul>
+          <li>
+            Tel.: <a href="tel:+380967199192">+38 (096) 719 91 92</a>{" "}
+            <a href="viber://chat?number=380967199192">[ Viber ]</a>
+          </li>
 
-        <li>
-          Telegram:{" "}
-          <a href="tg://resolve?domain=Serhii_web_dev">@Serhii_web_dev</a>
-        </li>
+          <li>
+            Telegram:{" "}
+            <a href="tg://resolve?domain=Serhii_web_dev">@Serhii_web_dev</a>
+          </li>
 
-        <li>
-          Email:{" "}
-          <a href="mailto:pylypenko.sn@gmail.com">pylypenko.sn@gmail.com</a>
-        </li>
+          <li>
+            Email:{" "}
+            <a href="mailto:pylypenko.sn@gmail.com">pylypenko.sn@gmail.com</a>
+          </li>
 
-        <li>Kyiv, Ukraine</li>
-      </ul>
+          <li>Kyiv, Ukraine</li>
+        </ul>
+      </div>
 
       {/* 
             About me
