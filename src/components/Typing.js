611dee936d7a69e522f9c783da266c0320818cc7
Serhii <39593488+PylSerg@users.noreply.github.com>
2022-10-12 15:00:20 +0300

    creates typing animation

--- src/components/Typing.js
+++ src/components/Typing.js
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Typing() {
-  return <></>;
+  return (
+    <div className="dot-block">
+      <div className="dot-1" />
+      <div className="dot-2" />
+      <div className="dot-3" />
+    </div>
+  );
 }
