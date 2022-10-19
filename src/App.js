9268e26b6307719fc9d3a61a4a97c4bd6eea2330
Serhii <39593488+PylSerg@users.noreply.github.com>
2022-10-19 22:23:54 +0300

    adds Projects component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
+import Projects from "./components/Projects";
 
 export default function App() {
   return (
     <div className="page">
       <Header />
       <AboutMe />
+      <Projects />
     </div>
   );
 }
