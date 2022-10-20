d9c281ed37605a2d455f98d064b14eff17eb9760
Serhii <39593488+PylSerg@users.noreply.github.com>
2022-10-20 23:07:46 +0300

    adds content and style to skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,28 @@
 import React from "react";
 
 export default function Skills() {
-  return;
+  return (
+    <section className="skills">
+      <h3>My skills</h3>
+
+      <ul className="skills__list">
+        <li className="skills__item">HTML5</li>
+        <li className="skills__item">CSS3/SCSS/SASS</li>
+        <li className="skills__item">JavaScript</li>
+        <li className="skills__item">React</li>
+        <li className="skills__item">React Hooks</li>
+        <li className="skills__item">React Hook Form</li>
+        <li className="skills__item">Redux</li>
+        <li className="skills__item">Redux Toolkit</li>
+        <li className="skills__item">ES6+</li>
+        <li className="skills__item">AJAX</li>
+        <li className="skills__item">Axios</li>
+        <li className="skills__item">REST API</li>
+        <li className="skills__item">NPM</li>
+        <li className="skills__item">Chrome DevTools</li>
+        <li className="skills__item">GitHub</li>
+        <li className="skills__item">Figma</li>
+      </ul>
+    </section>
+  );
 }
