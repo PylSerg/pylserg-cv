ac8bd7923950439d04213e15047aa44258c27525
Serhii <39593488+PylSerg@users.noreply.github.com>
2022-10-20 22:40:28 +0300

    adds Skills component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Header />
       <AboutMe />
       <Projects />
+      <Skills />
     </div>
   );
 }
