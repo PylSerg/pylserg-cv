13061715364d43452b2caf2639b7b58e253dc434
Serhii <39593488+PylSerg@users.noreply.github.com>
2022-10-21 00:16:33 +0300

    adds content and style

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,11 +1,35 @@
-import React from "react";
+import { useState } from "react";
+import { TfiViewListAlt, TfiViewGrid } from "react-icons/tfi";
 
 export default function Skills() {
+  const [listType, setListType] = useState({
+    type: "tiles",
+    style: "skills__list--tiles",
+  });
+
+  function changeListType() {
+    if (listType.type === "tiles")
+      return setListType({ type: "list", style: "skills__list--list" });
+
+    return setListType({ type: "tiles", style: "skills__list--tiles" });
+  }
+
   return (
     <section className="skills">
-      <h3>My skills</h3>
+      <div className="skills__title">
+        <h3>My skills</h3>
 
-      <ul className="skills__list">
+        <button
+          className="toggle-button"
+          type="button"
+          onClick={changeListType}
+        >
+          {listType.type === "tiles" && <TfiViewListAlt />}
+          {listType.type === "list" && <TfiViewGrid />}
+        </button>
+      </div>
+
+      <ul className={["skills__list", listType.style].join(" ")}>
         <li className="skills__item">HTML5</li>
         <li className="skills__item">CSS3/SCSS/SASS</li>
         <li className="skills__item">JavaScript</li>
