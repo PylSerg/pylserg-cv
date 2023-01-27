48230e153fd8df9bcba250b18566612ccca8b2f3
Serhii <39593488+PylSerg@users.noreply.github.com>
2023-01-27 20:49:25 +0200

    adds style for Languages

--- src/components/Langauges.js
+++ src/components/Langauges.js
@@ -12,11 +12,13 @@ export default function Languages() {
       <ul className="languages__list">
         <li className="languages__item">
           <img src={iconUK} alt="English" width={28} />
-          <b>English</b> - Pre-Intermediate
+          <snap className="languages__language">English</snap>
+          <snap className="languages__level"> - &nbsp; Pre-Intermediate</snap>
         </li>
         <li className="languages__item">
           <img src={iconUA} alt="Ukrainian" width={28} />
-          <b>Ukrainian</b> - Native
+          <snap className="languages__language">Ukrainian</snap>
+          <snap className="languages__level"> - &nbsp; Native</snap>
         </li>
       </ul>
     </section>
